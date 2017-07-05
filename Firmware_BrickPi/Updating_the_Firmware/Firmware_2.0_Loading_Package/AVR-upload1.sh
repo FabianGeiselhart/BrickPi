5c0ec918c5a2c71ad704685a38c7bdd7bcb220f6
Fabian Geiselhart <me@f4814n.de>
2017-07-05 14:12:39 +0200

    Use usbtinyisp programmer

--- Firmware_BrickPi/Updating_the_Firmware/Firmware_2.0_Loading_Package/AVR-upload1.sh
+++ Firmware_BrickPi/Updating_the_Firmware/Firmware_2.0_Loading_Package/AVR-upload1.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-avrdude -P usb -c avrispmkII -p m328p -e
+avrdude -P usb -c usbtiny -p m328p -e
 read -p "Press [Enter] key to continue ..."
-avrdude -P usb -c avrispmkII -p m328p -U lfuse:w:0xFF:m
+avrdude -P usb -c usbtiny -p m328p -U lfuse:w:0xFF:m
 read -p "Press [Enter] key to continue ..."
-avrdude -P usb -c avrispmkII -p m328p -U hfuse:w:0xDA:m
+avrdude -P usb -c usbtiny -p m328p -U hfuse:w:0xDA:m
 read -p "Press [Enter] key to continue ..."
-avrdude -P usb -c avrispmkII -p m328p -U efuse:w:0x05:m
+avrdude -P usb -c usbtiny -p m328p -U efuse:w:0x05:m
 read -p "Press [Enter] key to continue ..."
-avrdude -P usb -c avrispmkII -p m328p -U flash:w:ATmegaBOOT_168_atmega328.hex
+avrdude -P usb -c usbtiny -p m328p -U flash:w:ATmegaBOOT_168_atmega328.hex
 read -p "Press [Enter] key to continue ..."
-avrdude -P usb -c avrispmkII -p m328p -U flash:w:BrickPiFW_Compressed_Communication.cpp.hex
+avrdude -P usb -c usbtiny -p m328p -U flash:w:BrickPiFW_Compressed_Communication.cpp.hex
 read -p "Press [Enter] key to continue ..."
-avrdude -P usb -c avrispmkII -p m328p -U eeprom:w:BrickPi_EEPROM_UC1.hex
+avrdude -P usb -c usbtiny -p m328p -U eeprom:w:BrickPi_EEPROM_UC1.hex
+avrdude -P usb -c usbtiny -p m328p -U eeprom:v:BrickPi_EEPROM_UC1.hex
 read -p "Press [Enter] key to continue ..."
